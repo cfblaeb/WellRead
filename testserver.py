f58a1512d1a5f510ca825c3e7323ac2d0f136e2e
cfblaeb <lasse.ebdrup@gmail.com>
2019-02-16 23:34:00 +0100

    got a https testserver got a webcamtest working

---
+++ testserver.py
@@ -0,0 +1,13 @@
+#create server.pem:
+#openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
+
+
+import http.server, ssl
+
+server_address = ('localhost', 4443)
+httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket,
+                               server_side=True,
+                               certfile='server.pem',
+                               ssl_version=ssl.PROTOCOL_TLSv1)
+httpd.serve_forever()
